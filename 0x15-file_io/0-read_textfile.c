39a9e4c555b88e7e4533d9f571d2a88d000208a3
Ahmed-964 <abayoumi900@gmail.com>
2023-09-05 15:23:07 +0000

    try

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,28 +2,46 @@
 #include <stdlib.h>
 
 /**
- * read_textfile- Read text file print to STDOUT.
- * @filename: text file being read
- * @letters: number of letters to be read
- * Return: w- actual number of bytes read and printed
- *        0 when function fails or filename is NULL.
+ * read_textfile - Reads and prints a text file.
+ * @filename: The name of the file to read.
+ * @letters: The number of letters to read and print.
+ *
+ * Return: The actual number of letters read and printed, 0 on failure.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buf;
-	ssize_t fd;
-	ssize_t w;
-	ssize_t t;
+    if (filename == NULL)
+        return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
+    int fd = open(filename, O_RDONLY);
+    if (fd == -1)
+        return (0);
 
-	free(buf);
-	close(fd);
-	return (w);
+    char buffer[1024];
+    ssize_t bytes_read, bytes_written, total_written = 0;
+
+    while (letters > 0)
+    {
+        bytes_read = read(fd, buffer, sizeof(buffer));
+        if (bytes_read <= 0)
+            break;
+
+        if (bytes_read > (ssize_t)letters)
+            bytes_read = letters;
+
+        bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+
+        if (bytes_written == -1)
+        {
+            close(fd);
+            return (0);
+        }
+
+        total_written += bytes_written;
+        letters -= bytes_written;
+    }
+
+    close(fd);
+    return (total_written);
 }
 
